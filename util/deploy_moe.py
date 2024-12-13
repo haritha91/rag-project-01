e0aa841453460e8963f69d116ba45ae7b40de35d
haritha91 <harithalht@gmail.com>
2024-12-13 03:07:36 +0000

    change with rag with trace

--- util/deploy_moe.py
+++ util/deploy_moe.py
@@ -75,10 +75,10 @@ def deploy_flow(endpoint_name, deployment_name):
         name=deployment_name,
         endpoint_name=endpoint_name,
         model=Model(
-            name="ragflow",
+            name="ragwithtrace",
             path=flow_path,  # path to promptflow folder
             properties=[ # this enables the chat interface in the endpoint test tab
-                ["azureml.promptflow.source_flow_id", "ragflow"],
+                ["azureml.promptflow.source_flow_id", "ragwithtrace"],
                 ["azureml.promptflow.mode", "chat"],
                 ["azureml.promptflow.chat_input", "question"],
                 ["azureml.promptflow.chat_output", "answer"]
